4a1cce0ee6b46ccd563bac32fa2dce76579fe632
Mark Halls <mark.halls@gmail.com>
2019-12-12 20:53:47 -0700

    Add database and new seeds

--- data/seeds/1_recipes.js
+++ data/seeds/1_recipes.js
@@ -1,11 +1,12 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("recipe_ingredients")
+  return knex("recipes")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("recipe_ingredients").insert([
-        { recipe_id: 1, ingredient_id: 1, quantity: 2 }
+      return knex("recipes").insert([
+        { name: "Scrambled Eggs" },
+        { name: "Eggs Benedict" }
       ]);
     });
 };
